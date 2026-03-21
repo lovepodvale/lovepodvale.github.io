092280e361f4b2a934f9d6c6138b533b17da3569
lovepodvale <dthascist@gmail.com>
2026-03-22 02:32:29 +0300

    -

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3.12
 # -*- coding: utf-8 -*-
 
+import os
+
+
 with open('template.html', 'r') as f:
-    html = f.read()
+    init_html = f.read()
     f.close()
 
 with open('otkaz.txt', 'r') as f:
@@ -11,10 +14,19 @@ with open('otkaz.txt', 'r') as f:
 
 lines = text.strip().split('\n\n')
 lines = ['\t\t<p>' + line.replace('\n', '<br />') + '</p>\n' for line in lines]
+lines[0] = lines[0].replace('* * *', '* * * <a href="media.html">картинки</a>')
 
-html = html.replace('CONTENT', ''.join(lines))
+html = init_html.replace('CONTENT', ''.join(lines))
 html = html.replace('<main>	\t', '<main>\n\t\t')
 
 with open('index.html', 'w') as f:
+    f.write(html)
+    f.close()
+
+fnames = ['<a target="_blank" href="/media/{0}">{0}</a>'.format(fname) for fname in sorted(os.listdir('media'))]
+fnames = '<p>* * * <a href="index.html">текст</a></p><p>' + '<br />'.join(fnames) + '</p>'
+
+html = init_html.replace('CONTENT', fnames)
+with open('media.html', 'w') as f:
     f.write(html)
     f.close()
