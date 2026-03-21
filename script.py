9961a6c55931949984c867207aa36d8329ea145f
lovepodvale <dthascist@gmail.com>
2026-03-22 02:43:09 +0300

    -

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 
 
 with open('template.html', 'r') as f:
@@ -23,7 +24,12 @@ with open('index.html', 'w') as f:
     f.write(html)
     f.close()
 
-fnames = ['<a target="_blank" href="/media/{0}">{0}</a>'.format(fname) for fname in sorted(os.listdir('media'))]
+for fname in os.listdir('media'):
+    new_name = re.sub(r'^photo_(\d+)(@.*)$', lambda m: f"photo_{m.group(1).zfill(3)}{m.group(2)}", fname)
+    if new_name != fname:
+        os.rename('media/{0}'.format(fname), 'media/{0}'.format(new_name))
+
+fnames = ['<a target="_blank" href="/media/{0}">{0}</a>'.format(fname) for fname in reversed(sorted(os.listdir('media')))]
 fnames = '<p>* * * <a href="index.html">текст</a></p><p>' + '<br />'.join(fnames) + '</p>'
 
 html = init_html.replace('CONTENT', fnames)
